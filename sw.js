2d3886609ce8f2f7211093e8e02653080b2e112b
ino104 <nankai1972@yahoo.co.jp>
2026-07-16 11:44:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const C="rrq-v1";const CORE=["./","./index.html","./data.js","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(CORE))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>r))));
+const CACHE='chiiki-ver3-iphone-v1';
+const CORE=['./','./index.html','./questions.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}))));
