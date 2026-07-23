cacff06e77d9daab1668742e5bc5118b8031decc
ino104 <nankai1972@yahoo.co.jp>
2026-07-23 10:49:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chiiki-ver4-iphone-v1';
+const CACHE='chiiki-ver42-sprint1-v1';
 const CORE=['./','./index.html','./questions.js','./categories.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
